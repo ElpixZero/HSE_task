09bb187085a71e57ffae65c813821df51c173c89
Dager <nasurov.eldar@mail.ru>
2019-08-11 14:00:20 +0300

    Correcting card's message

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,7 +4,7 @@ import { Card } from 'semantic-ui-react';
 import './Card.scss';
 
 const CardComponent = ({countryName}) => {
-  const contentForCardDescription = `Ниже на графике Вы видите данные IMR по выше указанной стране с 2011 по 2017 год в зависимости от уровней: 'lower', 'median', 'upper'`;
+  const contentForCardDescription = `Ниже, на графике, Вы видите данные IMR по выше указанной стране с 2011 по 2017 год в зависимости от уровней: 'lower', 'median', 'upper'.`;
 
   return (
     <div className="card">
